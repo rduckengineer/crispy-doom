4282936104b9e15c32f99fc40f88c2cf9f890402
Rubber Duck Engineer <ruberduckengineering@gmail.com>
2022-02-12 01:10:46 +0100

    Simplify redundant test cases

--- src/doom/tests/savegame_test.cpp
+++ src/doom/tests/savegame_test.cpp
@@ -10,8 +10,7 @@ extern "C" {
 #include "catch.hpp"
 
 TEMPLATE_TEST_CASE("Writing in a file", "[write]", int8_t, int16_t, int32_t) {
-  GIVEN("An empty file")
-  {
+  GIVEN("An empty file") {
     FileStream<sizeof(TestType) + 1> file_stream{{}, OpenMode::Write};
     FileStream<128> error_stream{{}, OpenMode::Write};
 
@@ -21,41 +20,26 @@ TEMPLATE_TEST_CASE("Writing in a file", "[write]", int8_t, int16_t, int32_t) {
                                 take(10, random(std::numeric_limits<TestType>::min(),
                                                 std::numeric_limits<TestType>::max())));
 
-    AND_GIVEN("No prior error")
+    bool has_prior_error = GENERATE(true, false);
+    SaveGame saveg{file_stream.file(), has_prior_error, error_stream.file()};
+
+    WHEN("Writing in the stream")
     {
-      SaveGame saveg{file_stream.file(), false, error_stream.file()};
+      saveg.write<TestType>(expected);
 
-      WHEN("Writing in the stream")
+      THEN("There is no error after the write and the written value is correct")
       {
-        saveg.write<TestType>(expected);
-
-        THEN("There is no error after the write and the written value is correct")
-        {
-          CHECK_FALSE(saveg.error());
-          CHECK(file_stream.template as<TestType>() == expected);
-        }
-      }
-    }
-
-    AND_GIVEN("A prior error")
-    {
-      SaveGame saveg{file_stream.file(), true, error_stream.file()};
-      WHEN("Writing in the stream")
-      {
-        saveg.template write<TestType>(expected);
-        THEN("There is still an error after writing and the value was written")
-        {
-          CHECK(saveg.error());
-          CHECK(file_stream.template as<TestType>() == expected);
-        }
+        REQUIRE_FALSE(error_stream.has_error());
+        CHECK(error_stream.str().empty());
+        CHECK(saveg.error() == has_prior_error);
+        CHECK(file_stream.template as<TestType>() == expected);
       }
     }
   }
 }
 
 TEMPLATE_TEST_CASE("Writing in a file fails", "[write]", int8_t, int16_t, int32_t) {
-  GIVEN("A file with an error")
-  {
+  GIVEN("A file with an error") {
     FileStream<sizeof(TestType) + 1> file_stream{{0,0x37}, OpenMode::Read};
     FileStream<128> error_stream{{}, OpenMode::Write};
 
@@ -71,7 +55,6 @@ TEMPLATE_TEST_CASE("Writing in a file fails", "[write]", int8_t, int16_t, int32_
         {
           REQUIRE_FALSE(error_stream.has_error());
           CHECK(error_stream.str() == "saveg_write8: Error while writing save game\n");
-
           CHECK(file_stream.has_error());
           CHECK(saveg.error());
 
@@ -88,48 +71,28 @@ TEMPLATE_TEST_CASE("Writing in a file fails", "[write]", int8_t, int16_t, int32_
 
 // Read
 TEMPLATE_TEST_CASE("Reading in a file", "[read]", int8_t, int16_t, int32_t) {
-  GIVEN("A file")
-  {
+  GIVEN("A file") {
     FileStream<5> file_stream{{0x13,0x37, 0x42}, OpenMode::Read};
     FileStream<128> error_stream{{}, OpenMode::Write};
+    bool has_prior_error = GENERATE(true, false);
 
-    AND_GIVEN("No prior error")
+    SaveGame saveg{file_stream.file(), has_prior_error, error_stream.file()};
+
+    WHEN("Reading in the stream")
     {
-      SaveGame saveg{file_stream.file(), false, error_stream.file()};
-      WHEN("Reading in the stream")
+      auto result = saveg.read<TestType>();
+
+      THEN("There is no error after reading and the result variable has been set with the correct value")
       {
-        auto result = saveg.read<TestType>();
-
-        THEN("There is no error after reading and the result variable has been set with the correct value")
-        {
-          CHECK(saveg.error() == false);
-          CHECK(file_stream.template as<TestType>() == result);
-        }
-      }
-    }
-
-    AND_GIVEN("A prior error")
-    {
-      SaveGame saveg{file_stream.file(), false, error_stream.file()};
-      WHEN("Reading in the stream")
-      {
-        auto  result = saveg.template read<TestType>();
-
-        THEN("There is an error after the reading and the result variable has been set with the correct value")
-        {
-          REQUIRE_FALSE(error_stream.has_error());
-          CHECK(error_stream.str().empty());
-          CHECK(saveg.error() == false);
-          CHECK(file_stream.template as<TestType>() == result);
-        }
+        CHECK(saveg.error() == has_prior_error);
+        CHECK(file_stream.template as<TestType>() == result);
       }
     }
   }
 }
 
 TEMPLATE_TEST_CASE("Reading in a file with an error", "[read]", int8_t, int16_t, int32_t) {
-  GIVEN("A file")
-  {
+  GIVEN("A file") {
     FileStream<3> file_stream{{0x42, 0x52, 0x54}, OpenMode::Write};
     FileStream<128> error_stream{{}, OpenMode::Write};
 
@@ -138,7 +101,7 @@ TEMPLATE_TEST_CASE("Reading in a file with an error", "[read]", int8_t, int16_t,
       SaveGame saveg{file_stream.file(), false, error_stream.file()};
       WHEN("Reading in the stream")
       {
-        auto  result = saveg.template read<TestType>();
+        auto result = saveg.template read<TestType>();
 
         THEN("There is an error after the reading and the result is -1")
         {
@@ -146,7 +109,7 @@ TEMPLATE_TEST_CASE("Reading in a file with an error", "[read]", int8_t, int16_t,
           CHECK(error_stream.str() == "saveg_read8: Unexpected end of file while reading save game\n");
           CHECK(file_stream.has_error());
           CHECK(saveg.error());
-          CHECK(result == static_cast<int16_t>(-1));
+          CHECK(result == -1);
         }
       }
     }
