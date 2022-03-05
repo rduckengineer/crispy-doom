476f8d692615df5d6cb80bfd254a6352a3e22978
Rubber Duck Engineer <ruberduckengineering@gmail.com>
2022-03-05 16:52:29 +0100

    Separate impl from the interface for SaveGame

---
+++ src/doom/savegame/savegame.cpp
@@ -0,0 +1,115 @@
+#include "savegame.hpp"
+
+#include <limits>
+
+OpenMode SaveGame::openMode() const {
+  assert(m_context.read_file || m_context.write_file);
+  return m_context.read_file ? OpenMode::Read : OpenMode::Write;
+}
+
+bool SaveGame::stream_error() const noexcept {
+  if(m_context.read_file)
+    return m_context.read_file->fail();
+  else if(m_context.write_file)
+    return m_context.write_file->fail();
+  return true;
+}
+
+bool SaveGame::readInto(char *buffer) {
+  static constexpr size_t MAX_LINE_LENGTH = 260;
+  auto* file = m_context.read_file;
+  assert(file);
+  file->getline(buffer, MAX_LINE_LENGTH);
+
+  if(file->fail())
+    m_context.read_file->clear();
+
+  auto count = m_context.read_file->gcount();
+  return count != 0 && count != std::numeric_limits<std::streamsize>::max();
+}
+
+long SaveGame::currentPosition() const noexcept {
+  assert(m_context.read_file || m_context.write_file);
+  if(m_context.read_file)
+    return m_context.read_file->tellg();
+  else
+    return m_context.write_file->tellp();
+}
+
+void SaveGame::seekFromStart(long offset) {
+  assert(m_context.read_file || m_context.write_file);
+  if(m_context.read_file)
+    m_context.read_file->seekg(offset, std::ios::beg);
+  else if(m_context.write_file)
+    m_context.write_file->seekp(offset, std::ios::beg);
+}
+
+void SaveGame::seekFromEnd(long offset) {
+  assert(m_context.read_file || m_context.write_file);
+  if (m_context.read_file)
+    m_context.read_file->seekg(offset, std::ios::end);
+  else if (m_context.write_file)
+    m_context.write_file->seekp(offset, std::ios::end);
+}
+
+byte saveg_read8_from_context(SaveGameContext context)
+{
+  assert(context.read_file);
+
+  auto result = static_cast<byte>(context.read_file->get());
+  [[maybe_unused]] auto curPos = context.read_file->tellg();
+  if(context.read_file->fail() && !context.error) {
+    context.err_os << "saveg_read8: Unexpected end of file while "
+                      "reading save game\n";
+    context.error = true;
+  }
+  return result;
+}
+
+void saveg_write8_from_context(SaveGameContext context, byte value)
+{
+  assert(context.write_file);
+  context.write_file->put(static_cast<char>(value));
+  if(context.write_file->fail() && !context.error)
+  {
+    context.err_os << "saveg_write8: Error while writing save game\n";
+    context.error = true;
+  }
+}
+
+int16_t saveg_read16_from_context(SaveGameContext context)
+{
+  int16_t result;
+
+  result = saveg_read8_from_context(context);
+  result = static_cast<int16_t>(result | saveg_read8_from_context(context) << 8);
+
+  return result;
+}
+
+void  saveg_write16_from_context(SaveGameContext context, int16_t value)
+{
+  saveg_write8_from_context(context, value & 0xff);
+  saveg_write8_from_context(context, ((value >> 8) & 0xff));
+}
+
+
+int32_t saveg_read32_from_context(SaveGameContext context)
+{
+  int32_t result;
+
+  result = saveg_read8_from_context(context);
+  result |= saveg_read8_from_context(context) << 8;
+  result |= saveg_read8_from_context(context) << 16;
+  result |= saveg_read8_from_context(context) << 24;
+
+  return result;
+}
+
+void  saveg_write32_from_context(SaveGameContext context, int32_t value)
+{
+  saveg_write8_from_context(context, value & 0xff);
+  saveg_write8_from_context(context, (value >> 8) & 0xff);
+  saveg_write8_from_context(context, (value >> 16) & 0xff);
+  saveg_write8_from_context(context, (value >> 24) & 0xff);
+}
--- src/doom/savegame/savegame.hpp
+++ src/doom/savegame/savegame.hpp
@@ -5,18 +5,15 @@
 extern "C" {
 #include "doomtype.h"
 #include "p_saveg.h"
-#include "fcntl.h"
 }
 
 #include "savegame/open_mode.hpp"
 
 #include <iostream>
-#include <limits>
 #include <cassert>
 
 struct SaveGameContext
 {
-  //FILE* stream;
   bool& error;
   std::ostream& err_os;
   std::istream* read_file;
@@ -47,9 +44,28 @@ public:
     : m_context(context)
   {}
 
-  [[nodiscard]] OpenMode openMode() const {
-    assert(m_context.read_file || m_context.write_file);
-    return m_context.read_file ? OpenMode::Read : OpenMode::Write;
+  [[nodiscard]] OpenMode openMode() const;
+  [[nodiscard]] bool isOpen() const noexcept {
+    return m_context.read_file || m_context.write_file;
+  }
+  [[nodiscard]] bool error() const noexcept { return m_context.error; }
+  [[nodiscard]] bool stream_error() const noexcept;
+  [[nodiscard]] long currentPosition() const noexcept;
+
+  void seekFromStart(long offset);
+  void seekFromEnd(long offset);
+
+  bool readInto(char* buffer);
+  template <typename T>
+  [[nodiscard]] T read()
+  {
+    if constexpr (sizeof(T) == 1) {
+      return saveg_read8_from_context(m_context);
+    } else if constexpr (sizeof(T) == 2) {
+      return saveg_read16_from_context(m_context);
+    } else if constexpr (sizeof(T) == 4) {
+      return saveg_read32_from_context(m_context);
+    }
   }
 
   template <typename T>
@@ -67,66 +83,6 @@ public:
     } else {
       throw std::runtime_error("not implemented!");
     }
-  }
-
-  template <typename T>
-  [[nodiscard]] T read()
-  {
-    if constexpr (sizeof(T) == 1) {
-      return saveg_read8_from_context(m_context);
-    } else if constexpr (sizeof(T) == 2) {
-      return saveg_read16_from_context(m_context);
-    } else if constexpr (sizeof(T) == 4) {
-      return saveg_read32_from_context(m_context);
-    }
-  }
-
-  bool readInto(char* buffer) {
-    static constexpr size_t MAX_LINE_LENGTH = 260;
-    auto* file = m_context.read_file;
-    assert(file);
-    file->getline(buffer, MAX_LINE_LENGTH);
-
-    if(file->fail())
-      m_context.read_file->clear();
-
-    auto count = m_context.read_file->gcount();
-    return count != 0 && count != std::numeric_limits<std::streamsize>::max();
-  }
-
-  [[nodiscard]] bool isOpen() const noexcept {
-    return m_context.read_file || m_context.write_file;
-  }
-  [[nodiscard]] bool error() const noexcept { return m_context.error; }
-  [[nodiscard]] bool stream_error() const noexcept {
-    if(m_context.read_file)
-      return m_context.read_file->fail();
-    else if(m_context.write_file)
-      return m_context.write_file->fail();
-    return true;
-  }
-
-  long currentPosition() const {
-    assert(m_context.read_file || m_context.write_file);
-    if(m_context.read_file)
-      return m_context.read_file->tellg();
-    else
-      return m_context.write_file->tellp();
-  }
-
-  void seekFromStart(long offset) {
-    assert(m_context.read_file || m_context.write_file);
-    if(m_context.read_file)
-      m_context.read_file->seekg(offset, std::ios::beg);
-    else if(m_context.write_file)
-      m_context.write_file->seekp(offset, std::ios::beg);
-  }
-  void seekFromEnd(long offset) {
-    assert(m_context.read_file || m_context.write_file);
-    if (m_context.read_file)
-      m_context.read_file->seekg(offset, std::ios::end);
-    else if (m_context.write_file)
-      m_context.write_file->seekp(offset, std::ios::end);
   }
 
 private:
